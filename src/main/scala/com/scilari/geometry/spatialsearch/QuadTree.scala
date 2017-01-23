14cb82cf257cf3c12da997694142743d9c77525f
Ilari Vallivaara <ilari.vallivaara@gmail.com>
2017-01-23 08:01:03 +0200

    Refactored QuadTreeUtils to the same file. Added type annotations.

--- src/main/scala/com/scilari/geometry/spatialsearch/QuadTree.scala
+++ src/main/scala/com/scilari/geometry/spatialsearch/QuadTree.scala
@@ -3,6 +3,7 @@ import com.scilari.geometry.models.{AABB, Float2}
 import QuadTreeUtils._
 import com.scilari.geometry.spatialsearch.Tree.{Leaf, Node}
 
+import scala.collection.mutable
 import scala.collection.mutable.ListBuffer
 
 /**
@@ -19,14 +20,14 @@ class QuadNode[E <: Float2](
   bb: AABB,
   parent: QuadNode[E] = null
 ) extends QuadTree[E](bb, parent) with Node[Float2, QuadTree[E]]{
-  val children = Array[QuadTree[E]](
+  val children: Array[QuadTree[E]] = Array[QuadTree[E]](
     new QuadLeaf(topLeftAABB(this), this),
     new QuadLeaf(topRightAABB(this), this),
     new QuadLeaf(bottomLeftAABB(this), this),
     new QuadLeaf(bottomRightAABB(this), this)
   )
 
-  def add(elem: E) = {
+  def add(elem: E): QuadNode[E] = {
     val q = findQuadrant(elem, this)
     val child = getChild(q).add(elem)
     setChild(q, child)
@@ -44,11 +45,11 @@ class QuadLeaf[T <: Float2](
   bb: AABB,
   parent: QuadNode[T] = null
 ) extends QuadTree[T](bb, parent) with Leaf[Float2, T]{
-  val children = ListBuffer[T]()
+  val children: mutable.Buffer[T] = ListBuffer[T]()
 
 
   import QuadTree._
-  def add(elem: T) = {
+  def add(elem: T): QuadTree[T] = {
     children += elem
     if(children.size > nodeElementCapacity && width >= minNodeSize ) split() else this
   }
@@ -67,3 +68,52 @@ class QuadLeaf[T <: Float2](
 
 
 }
+
+object QuadTreeUtils {
+  val topLeftIndex = 0
+  val topRightIndex = 1
+  val bottomLeftIndex = 2
+  val bottomRightIndex = 3
+
+
+  def topLeftAABB(b: AABB) = AABB(b.minPoint.x, b.centerY, b.centerX, b.maxPoint.y)
+  def topRightAABB(b: AABB) = AABB(b.center, b.maxPoint)
+  def bottomLeftAABB(b: AABB) = AABB(b.minPoint, b.center)
+  def bottomRightAABB(b: AABB) = AABB(b.centerX, b.minPoint.y, b.maxPoint.x, b.centerY)
+
+  def topLeftAABB(b: AABB, centerPoint: Float2) = AABB(b.minPoint.x, centerPoint.y, centerPoint.x, b.maxPoint.y)
+  def topRightAABB(b: AABB, centerPoint: Float2) = AABB(centerPoint, b.maxPoint)
+  def bottomLeftAABB(b: AABB, centerPoint: Float2) = AABB(b.minPoint, centerPoint)
+  def bottomRightAABB(b: AABB, centerPoint: Float2) = AABB(centerPoint.x, b.minPoint.y, b.maxPoint.x, centerPoint.y)
+
+  def quadrantByIndex(b: AABB, index: Int): AABB = {
+    index match{
+      case `topLeftIndex` => topLeftAABB(b)
+      case `topRightIndex` => topRightAABB(b)
+      case `bottomLeftIndex` => bottomLeftAABB(b)
+      case `bottomRightIndex` => bottomRightAABB(b)
+    }
+  }
+
+  // Computes the AABB that has b as its children and that is most towards given point
+  def enclosingAABB(point: Float2, b: AABB): AABB ={
+    val corner = b.closestCorner(point)
+    new AABB(center = corner, halfWidth = b.width)
+  }
+
+  def findQuadrant(point: Float2, b: AABB): Int = {
+    // This branch prediction friendly implementation seems to be over three times faster than simple if-based one
+    val rowIncrement = if(point.x <= b.centerX) 0 else 1
+    val columnIncrement = if(point.y <= b.centerY) 2 else 0
+    rowIncrement + columnIncrement
+  }
+
+
+  def findQuadrant(point: Float2, centerPoint: Float2): Int = {
+    val rowIncrement = if(point.x <= centerPoint.x) 0 else 1
+    val columnIncrement = if(point.y <= centerPoint.y) 2 else 0
+    rowIncrement + columnIncrement
+  }
+
+}
+
