ec1fbea6862926ee3c17e98e2d83f9e9909dd0c9
vallivaara <ilari.vallivaara@indooratlas.com>
2017-02-14 16:32:48 +0200

    Equals and hashCode implementations. Simple description.

--- src/main/scala/com/scilari/geometry/models/DataPoint.scala
+++ src/main/scala/com/scilari/geometry/models/DataPoint.scala
@@ -1,8 +1,18 @@
 package com.scilari.geometry.models
 
 /**
- * Created by iv on 8/15/2015.
+  * Data point for holding two-dimensional data
+  * Created by iv on 8/15/2015.
  */
 class DataPoint[E](x: Float, y: Float, var data: E = null) extends Float2(x, y) {
   def this(p: Float2, data: E) = this(p.x, p.y, data)
+
+  override def equals(that: Any): Boolean ={
+    isInstanceOf[DataPoint[E]] && {
+      val thatDp = that.asInstanceOf[DataPoint[E]]
+      equalCoordinates(thatDp) && data == thatDp.data
+    }
+  }
+
+  override def hashCode(): Int = 37*super.hashCode() + data.hashCode()
 }
