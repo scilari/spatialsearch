c5cc975eb200ab07dc1b2e48c641281758d5c14b
Ilari Vallivaara <ilari.vallivaara@gmail.com>
2017-09-10 15:40:54 +0300

    Fix comments. Rename parameters.

--- src/main/scala/com/scilari/geometry/models/HalfPlaneObject.scala
+++ src/main/scala/com/scilari/geometry/models/HalfPlaneObject.scala
@@ -1,13 +1,14 @@
 package com.scilari.geometry.models
 
 /**
-  * An object that can intersect a half-plane. The half-plane is defined by its normal vector's endpoints.
+  * An object that can intersect a half-plane. The half-plane is defined by the normal vector of the separating line
+  * given by its endpoints.
   */
 trait HalfPlaneObject {
   def pointDeepestInHalfPlane(normal: Float2): Float2
-  def intersectsHalfPlane(normalEndPointInside: Float2, normalStartPointOnBorder: Float2): Boolean  = {
-    val normal = normalStartPointOnBorder - normalEndPointInside
-    val toBorder = normalStartPointOnBorder - pointDeepestInHalfPlane(normal)
+  def intersectsHalfPlane(normalEndPointInside: Float2, normalStartPointOnLine: Float2): Boolean  = {
+    val normal = normalStartPointOnLine - normalEndPointInside
+    val toBorder = normalStartPointOnLine - pointDeepestInHalfPlane(normal)
     toBorder.dot(normal) >= 0
   }
 }
