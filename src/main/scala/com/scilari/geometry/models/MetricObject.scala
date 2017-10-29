0692cec3af8080447ee4ce5ee99c7c3b49075c3d
Ilari Vallivaara <ilari.vallivaara@gmail.com>
2017-10-29 16:49:50 +0200

    Functionality to check for zero distance (for optimization).

--- src/main/scala/com/scilari/geometry/models/MetricObject.scala
+++ src/main/scala/com/scilari/geometry/models/MetricObject.scala
@@ -7,5 +7,5 @@ package com.scilari.geometry.models
 trait MetricObject[T] {
   def distanceSq(point: T): Float
   def distance(point: T): Float = math.sqrt(distanceSq(point)).toFloat
-
+  def zeroDistance(point: T): Boolean = distanceSq(point) == 0f
 }
