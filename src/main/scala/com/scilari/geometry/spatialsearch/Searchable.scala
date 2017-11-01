6faf0a4dce7b486386333576a0da2a82348a8b63
vallivaara <ilari.vallivaara@indooratlas.com>
2017-11-01 14:46:35 +0200

    Initial commit.

---
+++ src/main/scala/com/scilari/geometry/spatialsearch/Searchable.scala
@@ -0,0 +1,21 @@
+package com.scilari.geometry.spatialsearch
+
+import com.scilari.geometry.models.Float2
+
+trait Searchable[E] {
+
+  def knnSearch(queryPoint: Float2, k: Int): Seq[E]
+
+  def rangeSearch(queryPoint: Float2, r: Float): Seq[E]
+
+  def polygonalSearch(queryPoint: Float2): Seq[E]
+
+  def knnSearchWithCondition(queryPoint: Float2, k: Int, condition: E => Boolean): Seq[E]
+
+  def isEmptyRange(queryPoint: Float2, r: Float): Boolean = {
+    rangeSearch(queryPoint, r).isEmpty
+  }
+
+  def nonEmptyRange(queryPoint: Float2, r: Float): Boolean = !isEmptyRange(queryPoint, r)
+
+}
