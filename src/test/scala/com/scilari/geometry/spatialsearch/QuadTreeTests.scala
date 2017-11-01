f05f7a18fa286a397eeaf1334c1e37a077bf8154
vallivaara <ilari.vallivaara@indooratlas.com>
2017-11-01 15:15:05 +0200

    Name refactoring.

--- src/test/scala/com/scilari/geometry/spatialsearch/QuadTreeTests.scala
+++ src/test/scala/com/scilari/geometry/spatialsearch/QuadTreeTests.scala
@@ -1,13 +1,12 @@
 package com.scilari.geometry.spatialsearch
 
 import com.scilari.geometry.models.{AABB, DataPoint, Float2}
-import com.scilari.geometry.spatialsearch.SearchTree.Concrete
 import com.scilari.geometry.spatialsearch.TestResources.{City, cityData}
 import com.scilari.geometry.spatialsearch.quadtree.{Parameters, QuadTree}
 
 class QuadTreeTests extends TreeTests {
   override def treeName: String = "QuadTree"
-  override def createEmptyUnitTree: Concrete[Float2] = QuadTree[Float2](AABB.unit, Parameters(nodeElementCapacity = 16))
-  override def createFilledTree: Concrete[Float2] = QuadTree(points, Parameters(nodeElementCapacity = 16))
-  override def createCityTree: Concrete[DataPoint[City]] = QuadTree[DataPoint[City]](cityData, Parameters(nodeElementCapacity = 16))
+  override def createEmptyUnitTree: SearchTree[Float2] = QuadTree[Float2](AABB.unit, Parameters(nodeElementCapacity = 16))
+  override def createFilledTree: SearchTree[Float2] = QuadTree(points, Parameters(nodeElementCapacity = 16))
+  override def createCityTree: SearchTree[DataPoint[City]] = QuadTree[DataPoint[City]](cityData, Parameters(nodeElementCapacity = 16))
 }
