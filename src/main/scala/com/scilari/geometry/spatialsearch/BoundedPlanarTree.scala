c105e74ca306683158b00b1b33adb75798f64892
Ilari Vallivaara <ilari.vallivaara@gmail.com>
2017-11-02 22:28:49 +0200

    Extract common base class.

---
+++ src/main/scala/com/scilari/geometry/spatialsearch/BoundedPlanarTree.scala
@@ -0,0 +1,12 @@
+package com.scilari.geometry.spatialsearch
+
+import com.scilari.geometry.models.{AABB, Float2}
+
+trait BoundedPlanarTree[E <: Float2] extends Tree[Float2, E] {
+  override type BaseType <: AABB with Base
+
+  abstract class BoundedBase(bb: AABB) extends AABB(bb) with Base{
+    override def contains(p: Float2): Boolean = super[AABB].contains(p)
+    override def toString(): String = "Bounded Tree: " + super[AABB].toString()
+  }
+}
