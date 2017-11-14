97618db39787a927f4010850c8c998ce8955faf5
Ilari Vallivaara <ilari.vallivaara@gmail.com>
2017-11-14 10:26:22 +0200

    Initial commit.

---
+++ src/test/scala/com/scilari/geometry/spatialsearch/QueueTests.scala
@@ -0,0 +1,57 @@
+package com.scilari.geometry.spatialsearch
+
+import com.scilari.geometry.spatialsearch.queues.{FloatHeap, FloatPriorityQueue, JavaPriorityQueueWrapper, ScalaPriorityQueueWrapper}
+import org.scalatest.{FlatSpec, Matchers}
+
+trait QueueTests extends FlatSpec with Matchers {
+  def queueName: String
+  def createEmptyQueue: FloatPriorityQueue[String]
+
+  queueName should "accept and return the elements in correct order" in {
+          val heap = createEmptyQueue
+          heap.enqueue(0.4f, "D")
+          heap.enqueue(0.5f, "E")
+          heap.enqueue(0.2f, "B")
+          heap.enqueue(0.1f, "A")
+          heap.enqueue(0.3f, "C")
+          heap.enqueue(-0.4f, "d")
+          heap.enqueue(-0.5f, "e")
+          heap.enqueue(-0.2f, "b")
+          heap.enqueue(-0.1f, "a")
+          heap.enqueue(-0.3f, "c")
+
+
+    heap.size should be (10)
+    assert(heap.nonEmpty)
+
+    val correctOrder = Seq("e", "d", "c", "b", "a", "A", "B", "C", "D", "E")
+    val dequeued = for(_ <- 0 until heap.size) yield {
+      heap.dequeueValue()
+    }
+
+    dequeued.zip(correctOrder).foreach{ case(s1, s2) =>
+      println(s1 + " " + s2)
+        s1 should be (s2)
+    }
+
+    heap.size should be (0)
+    assert(heap.isEmpty)
+
+  }
+
+}
+
+class FloatHeapTest extends QueueTests {
+  def queueName: String = "FloatHeap"
+  override def createEmptyQueue: FloatPriorityQueue[String] = new FloatHeap[String](2)
+}
+
+class JavaPriorityQueueWrapperTest extends QueueTests {
+  def queueName: String = "JavaPriorityQueueWrapper"
+  override def createEmptyQueue: FloatPriorityQueue[String] = new JavaPriorityQueueWrapper[String]()
+}
+
+class ScalaPriorityQueueWrapperTest extends QueueTests {
+  def queueName: String = "ScalaPriorityQueueWrapper"
+  override def createEmptyQueue: FloatPriorityQueue[String] = new ScalaPriorityQueueWrapper[String]()
+}
