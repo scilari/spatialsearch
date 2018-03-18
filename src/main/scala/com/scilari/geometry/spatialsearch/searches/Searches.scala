95e7ec5fb6829cac0742a84b84710d93c8a4f798
Ilari Vallivaara <ilari.vallivaara@gmail.com>
2018-03-19 00:11:43 +0200

    Fix type parameters

--- src/main/scala/com/scilari/geometry/spatialsearch/searches/Searches.scala
+++ src/main/scala/com/scilari/geometry/spatialsearch/searches/Searches.scala
@@ -1,3 +1,5 @@
 package com.scilari.geometry.spatialsearch.searches
 
-trait Searches[P, E] extends BasicSearches[P, E] with PolygonalSearches[P, E]
+import com.scilari.geometry.models.Float2
+
+trait Searches[P <: Float2, E <: Float2] extends BasicSearches[P, E] with PolygonalSearches[P, E]
