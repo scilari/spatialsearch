d24eae7f913deda1aa2b10f69b2a156d2f0ead7b
vallivaara <ilari.vallivaara@indooratlas.com>
2018-07-31 16:57:12 +0300

    Initial commit.

---
+++ src/main/scala/com/scilari/geometry/spatialsearch/searches/SearchesImpl.scala
@@ -0,0 +1,48 @@
+package com.scilari.geometry.spatialsearch.searches
+
+import com.scilari.geometry.models.{AABB, Float2}
+
+/**
+  * Concrete implementations with distance functions for searches member variables
+  * @tparam E Element type
+  */
+trait SearchesImpl[E <: Float2] extends TreeSearches.Combined[Float2, E] {
+  import SearchesImpl._
+  val basicSearches: BasicSearches[Float2, E] = new PointDistances[E]
+  val polygonalSearches: PolygonalSearches[Float2, E] = new PointDistances[E]
+  val seqSearches: BasicSearches[Seq[Float2], E] = new SeqDistances[Seq[Float2], E]
+}
+
+object SearchesImpl{
+    class PointDistances[E <: Float2] extends BasicSearches[Float2, E] with PolygonalSearches[Float2, E] {
+      override type BaseType <: Base with AABB
+      override def elemDist(p: Float2, e: E): Float = p.distanceSq(e)
+      override def nodeDist(p: Float2, n: BaseType): Float = n.distanceSq(p)
+    }
+
+    class SeqDistances[P <: Seq[Float2], E <: Float2] extends BasicSearches[P, E] {
+      override type BaseType <: Base with AABB
+
+      override def elemDist(ps: P, e: E): Float = {
+        var minD = Float.MaxValue
+        var i = 0
+        val n = ps.size
+        while(i < n){
+          minD = math.min(minD, e.distanceSq(ps(i)))
+          i += 1
+        }
+        minD
+      }
+
+      override def nodeDist(ps: P, node: BaseType): Float = {
+        var minD: Float = Float.MaxValue
+        var i = 0
+        val n = ps.size
+        while(i < n && minD > 0f){  // early exit with distance == 0f
+          minD = math.min(minD, node.distanceSq(ps(i)))
+          i += 1
+        }
+        minD
+      }
+    }
+}
