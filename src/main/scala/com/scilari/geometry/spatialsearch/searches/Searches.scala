3b9602fe2563530cebd71114d6609950f96529c2
vallivaara <ilari.vallivaara@indooratlas.com>
2018-08-01 14:10:31 +0300

    Satisfy polygonalSearch generic parameter type.

--- src/main/scala/com/scilari/geometry/spatialsearch/searches/Searches.scala
+++ src/main/scala/com/scilari/geometry/spatialsearch/searches/Searches.scala
@@ -5,7 +5,7 @@ import com.scilari.geometry.spatialsearch.core.Tree
 
 trait Searches[P, E <: Float2] extends Tree[E] {
   val basicSearches: BasicSearches[P, E]
-  val polygonalSearches: PolygonalSearches[Float2, E] // TODO
+  val polygonalSearches: PolygonalSearches[P with Float2, E]
   val seqSearches: BasicSearches[Seq[P], E]
 }
 
