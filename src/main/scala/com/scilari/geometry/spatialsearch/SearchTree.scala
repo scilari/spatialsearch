d297406fb4c1a6f36c5d8130dc729a4f94227fef
scilari <ilari.vallivaara@gmail.com>
2018-09-01 12:11:30 +0300

    Use SearchTree object directly for default factory methods. Add method to safely cast to quadTree (building it if needed).

--- src/main/scala/com/scilari/geometry/spatialsearch/SearchTree.scala
+++ src/main/scala/com/scilari/geometry/spatialsearch/SearchTree.scala
@@ -1,23 +1,20 @@
 package com.scilari.geometry.spatialsearch
 
 import com.scilari.geometry.models.{AABB, Float2}
-import com.scilari.geometry.spatialsearch.trees.quadtree.Parameters
+import com.scilari.geometry.spatialsearch.trees.quadtree.{Parameters, QuadTree}
 
-trait SearchTree[E] extends Searchable[E] with SpatialContainer[E]
+
+trait SearchTree[E <: Float2] extends Searchable[E] with SpatialContainer[E] {
+  def toQuadTree: QuadTree[E] = this match {
+    case t: QuadTree[E] => t
+    case t: SearchTree[E] => QuadTree[E](t.elements)
+  }
+
+}
 
 object SearchTree{
-  import trees.quadtree.{QuadTree => QT}
-  import trees.multitree.{MultiTree => MT}
-
-  object QuadTree{
-    def apply[E <: Float2](bb: AABB, p: Parameters): SearchTree[E] = QT[E](bb, p)
-    def apply[E <: Float2](bb: AABB): SearchTree[E] = QT[E](bb)
-    def apply[E <: Float2](elements: Seq[E], p: Parameters): SearchTree[E] = QT[E](elements, p)
-    def apply[E <: Float2](elements: Seq[E]): SearchTree[E] = QT[E](elements)
-  }
-
-  object MultiTree{
-    def apply[E <: Float2](trees: Seq[QT[E]]): Searchable[E] = MT[E](trees)
-  }
-
+    def apply[E <: Float2](bb: AABB, p: Parameters): SearchTree[E] = QuadTree[E](bb, p)
+    def apply[E <: Float2](bb: AABB): SearchTree[E] = QuadTree[E](bb)
+    def apply[E <: Float2](elements: Seq[E], p: Parameters): SearchTree[E] = QuadTree[E](elements, p)
+    def apply[E <: Float2](elements: Seq[E]): SearchTree[E] = QuadTree[E](elements)
 }
