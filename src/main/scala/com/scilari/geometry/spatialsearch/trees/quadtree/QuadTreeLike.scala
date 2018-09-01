6c796a58cc3ff90434609b8bd2c3835925547c7e
scilari <ilari.vallivaara@gmail.com>
2018-09-01 12:10:06 +0300

    Modify member class privacy.

--- src/main/scala/com/scilari/geometry/spatialsearch/trees/quadtree/QuadTreeLike.scala
+++ src/main/scala/com/scilari/geometry/spatialsearch/trees/quadtree/QuadTreeLike.scala
@@ -8,42 +8,44 @@ import scala.collection.mutable
 import scala.collection.mutable.ArrayBuffer
 
 trait QuadTreeLike[E <: Float2] extends BoundedSearchTree[E]{
-    type NodeType = BoundedNode
-    type BranchType = QuadBranch
-    type LeafType = QuadLeaf
+  type NodeType = BoundedNode
+  type BranchType = QuadBranch
+  type LeafType = QuadLeaf
 
-    class QuadBranch(
-      bb: AABB,
-      val parent: Option[BranchType] = None,
-      parameters: Parameters
-    ) extends NodeType(bb) with Branch {
+  protected class QuadBranch(
+    bb: AABB,
+    val parent: Option[BranchType] = None,
+    parameters: Parameters
+  ) extends NodeType(bb) with Branch {
 
-      val children: Array[NodeType] = {
-        val parent = Some(this)
-        Array[NodeType](
-          new LeafType(topLeftAABB(this), parent, parameters),
-          new LeafType(topRightAABB(this), parent, parameters),
-          new LeafType(bottomLeftAABB(this), parent, parameters),
-          new LeafType(bottomRightAABB(this), parent, parameters)
-        )
-      }
-
-      def findChildIndex(elem: E): Int = findQuadrant(elem, this)
+    val children: Array[NodeType] = {
+      val parent = Some(this)
+      Array[NodeType](
+        new LeafType(topLeftAABB(this), parent, parameters),
+        new LeafType(topRightAABB(this), parent, parameters),
+        new LeafType(bottomLeftAABB(this), parent, parameters),
+        new LeafType(bottomRightAABB(this), parent, parameters)
+      )
     }
 
-    class QuadLeaf(
-      bb: AABB,
-      val parent: Option[BranchType] = None,
-      parameters: Parameters
-    ) extends NodeType(bb) with Leaf{
-      val elements: mutable.Buffer[E] = new ArrayBuffer[E]()
+    def findChildIndex(elem: E): Int = findQuadrant(elem, this)
 
-      def splitCondition: Boolean =
-        elements.lengthCompare(parameters.nodeElementCapacity) > 0 && width > parameters.minNodeSize
+  }
 
-      def toNode: BranchType = new BranchType(this, this.parent, parameters)
 
-    }
+  protected class QuadLeaf(
+    bb: AABB,
+    val parent: Option[BranchType] = None,
+    parameters: Parameters
+  ) extends NodeType(bb) with Leaf{
+    val elements: mutable.Buffer[E] = new ArrayBuffer[E]()
+
+    def splitCondition: Boolean =
+      elements.lengthCompare(parameters.nodeElementCapacity) > 0 && width > parameters.minNodeSize
+
+    def toNode: BranchType = new BranchType(this, this.parent, parameters)
+
+  }
 }
 
 
