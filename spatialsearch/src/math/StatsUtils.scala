db47af384df89d0769ee7b078225b2f736389abf
Ilari Vallivaara <ilari.vallivaara@gmail.com>
2021-10-28 23:38:30 +0100

    Make utils more general

--- spatialsearch/src/math/StatsUtils.scala
+++ spatialsearch/src/math/StatsUtils.scala
@@ -2,13 +2,13 @@ package com.scilari.math
 
 object StatsUtils {
 
-  def mean(xs: Seq[Double]): Double = xs.sum / xs.size
+  def mean(xs: Iterable[Double]): Double = xs.sum / xs.size
 
-  def variance(xs: Seq[Double]): Double = {
+  def variance(xs: Iterable[Double]): Double = {
     val m = mean(xs)
     val dxs = xs.map { x => (x - m) * (x - m) }
     mean(dxs)
   }
 
-  def std(xs: Seq[Double]): Double = math.sqrt(variance(xs))
+  def std(xs: Iterable[Double]): Double = math.sqrt(variance(xs))
 }
