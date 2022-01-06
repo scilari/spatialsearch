fa473061220af4747d278131aa18463c3e52f4c5
Ilari Vallivaara <ilari.vallivaara@gmail.com>
2022-01-06 20:31:50 +0000

    Use concrete collections

--- spatialsearch/src/geometry/spatialsearch/queues/FloatMaxHeapK.scala
+++ spatialsearch/src/geometry/spatialsearch/queues/FloatMaxHeapK.scala
@@ -2,29 +2,28 @@ package com.scilari.geometry.spatialsearch.queues
 
 import collection.mutable.Buffer
 
-
-
-/**
- * Optimized version for computing k smallest values from a stream of n >> k values. Skips enqueue operations when the
- * queue is full and the inserted element is larger than the current max.
- * @param k The number of smallest values we want to compute.
- * @tparam E Element type parameter.
- */
-class FloatMaxHeapK[E](k: Int) extends FloatMinHeap[E](k){
+/** Optimized version for computing k smallest values from a stream of n >> k values. Skips enqueue
+  * operations when the queue is full and the inserted element is larger than the current max.
+  * @param k
+  *   The number of smallest values we want to compute.
+  * @tparam E
+  *   Element type parameter.
+  */
+class FloatMaxHeapK[E](k: Int) extends FloatMinHeap[E](k) {
   override def cmp(key1: Float, key2: Float): Boolean = key1 > key2
-  
+
   override def enqueue(key: Float, value: E): Unit = {
-    if(size + 1 < capacity){
+    if (size + 1 < capacity) {
       super.enqueue(key, value)
-    } else if(key < headKey){
+    } else if (key < headKey) {
       bubbleDown(key, value)
     } // else just discard
   }
-  
+
   override def maxKey = headKey
-  
+
   override def minKey = keys.min
-  
+
   // Use only peekValues
   override def dequeue(): FloatKey[E] = ???
 
--- spatialsearch/src/geometry/spatialsearch/queues/FloatMinHeap.scala
+++ spatialsearch/src/geometry/spatialsearch/queues/FloatMinHeap.scala
@@ -1,8 +1,7 @@
 package com.scilari.geometry.spatialsearch.queues
 
 import scala.annotation.tailrec
-import scala.collection.mutable
-import scala.collection.mutable.Buffer
+import scala.collection.mutable.ArrayBuffer
 
 class FloatMinHeap[E](initialCapacity: Int = FloatMinHeap.defaultInitialCapacity) {
 
@@ -10,7 +9,7 @@ class FloatMinHeap[E](initialCapacity: Int = FloatMinHeap.defaultInitialCapacity
   private[this] def left(k: Int): Int = k << 1
   private[this] def right(k: Int): Int = (k << 1) + 1
   private[this] def parent(k: Int): Int = k >> 1
-  
+
   protected var capacity: Int = initialCapacity + 1
   protected var values = new Array[Any](capacity)
   protected var keys = new Array[Float](capacity)
@@ -18,26 +17,25 @@ class FloatMinHeap[E](initialCapacity: Int = FloatMinHeap.defaultInitialCapacity
   private[this] var maxIndex = 0
   protected def cmp(key1: Float, key2: Float): Boolean = key1 < key2
 
-  keys(0) = if(cmp(0f, 1f)) Float.MinValue else Float.MaxValue // use as a sentinel in bubbleUp
+  keys(0) = if (cmp(0f, 1f)) Float.MinValue else Float.MaxValue // use as a sentinel in bubbleUp
 
-  private[this] def move(to: Int, from: Int): Unit ={
+  private[this] def move(to: Int, from: Int): Unit = {
     keys(to) = keys(from)
     values(to) = values(from)
   }
 
-  private[this] def update(i: Int, key: Float, value: E): Unit ={
+  private[this] def update(i: Int, key: Float, value: E): Unit = {
     keys(i) = key
     values(i) = value
   }
 
   def enqueue(key: Float, value: E): Unit = {
     maxIndex += 1
-    if(maxIndex == capacity) doubleCapacity()
+    if (maxIndex == capacity) doubleCapacity()
     bubbleUp(key, value)
   }
 
-
-  protected[this] def doubleCapacity(): Unit ={
+  protected[this] def doubleCapacity(): Unit = {
     capacity *= 2
     val newValues = new Array[Any](capacity)
     val newKeys = new Array[Float](capacity)
@@ -61,39 +59,38 @@ class FloatMinHeap[E](initialCapacity: Int = FloatMinHeap.defaultInitialCapacity
     headValue
   }
 
-  def peekValuesToBuffer(b: Buffer[E]): Buffer[E] = {
+  def peekValuesToBuffer(b: ArrayBuffer[E]): ArrayBuffer[E] = {
     var i = firstIndex
-    while(i <= maxIndex){
+    while (i <= maxIndex) {
       b += values(i).asInstanceOf[E]
       i += 1
     }
     b
   }
 
-  def peekValues: mutable.Buffer[E] = peekValuesToBuffer(Buffer[E]())
-  
- 
-  def getKeysAndValues: mutable.Buffer[FloatKey[E]] = {
-    val b = mutable.Buffer[Any]()
+  def peekValues: ArrayBuffer[E] = peekValuesToBuffer(ArrayBuffer[E]())
+
+  def getKeysAndValues: ArrayBuffer[FloatKey[E]] = {
+    val b = ArrayBuffer[Any]()
     var i = firstIndex
-    while(i < maxIndex){  // TODO: check if this is ok
+    while (i < maxIndex) { // TODO: check if this is ok
       b += FloatKey(keys(i), values(i))
       i += 1
     }
-    b.asInstanceOf[mutable.Buffer[FloatKey[E]]]
+    b.asInstanceOf[ArrayBuffer[FloatKey[E]]]
   }
 
-  private[this] def popFirst(): Unit ={
+  private[this] def popFirst(): Unit = {
     maxIndex -= 1
     bubbleDown(keys(maxIndex + 1), getValue(maxIndex + 1))
   }
 
-  private[this] def bubbleUp(key: Float, value: E): Unit ={
+  private[this] def bubbleUp(key: Float, value: E): Unit = {
 
     @tailrec
     def makeRoomUp(pos: Int): Int = {
       val par = parent(pos)
-      if(cmp(key, keys(par))){
+      if (cmp(key, keys(par))) {
         move(pos, par)
         makeRoomUp(par)
       } else {
@@ -104,21 +101,21 @@ class FloatMinHeap[E](initialCapacity: Int = FloatMinHeap.defaultInitialCapacity
     update(makeRoomUp(maxIndex), key, value)
   }
 
-  protected def bubbleDown(key: Float, value: E): Unit ={
+  protected def bubbleDown(key: Float, value: E): Unit = {
     @tailrec
     def makeRoomDown(k: Int): Int = {
       val l = left(k)
       val r = l + 1
 
-      if(l > maxIndex) {
+      if (l > maxIndex) {
         k
       } else {
-        val child = if(l != maxIndex){
-          if(cmp(keys(l), keys(r))) l else r
+        val child = if (l != maxIndex) {
+          if (cmp(keys(l), keys(r))) l else r
         } else {
           l
         }
-        
+
         if (cmp(keys(child), key)) {
           move(k, child)
           makeRoomDown(child)
@@ -130,11 +127,11 @@ class FloatMinHeap[E](initialCapacity: Int = FloatMinHeap.defaultInitialCapacity
 
     update(makeRoomDown(firstIndex), key, value)
   }
-  
+
   def peek: E = getValue(firstIndex)
-  
+
   def head: FloatKey[E] = FloatKey(keys(firstIndex), getValue(firstIndex))
-  
+
   protected def headKey = keys(firstIndex)
 
   def minKey: Float = headKey
@@ -150,8 +147,8 @@ class FloatMinHeap[E](initialCapacity: Int = FloatMinHeap.defaultInitialCapacity
   def clear(): Unit = maxIndex = 0
 }
 
-object FloatMinHeap{
-  def apply[E](k: Float, e: E, initialCapacity: Int = defaultInitialCapacity): FloatMinHeap[E] ={
+object FloatMinHeap {
+  def apply[E](k: Float, e: E, initialCapacity: Int = defaultInitialCapacity): FloatMinHeap[E] = {
     val h = new FloatMinHeap[E](initialCapacity)
     h.enqueue(k, e)
     h
