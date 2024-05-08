e22d7b9605dffb90f7728cdabe9403ac3c5cde51
Ilari Vallivaara <ilari.vallivaara@gmail.com>
2024-05-08 20:11:31 +0100

    Rename

--- spatialsearch/src/quadtree/UnionTree.scala
+++ spatialsearch/src/quadtree/UnionTree.scala
@@ -5,10 +5,10 @@ import Tree.Node
 import com.scilari.geometry.models.{AABB, Position}
 import scala.collection.mutable.{ArrayBuffer, ArraySeq}
 
-final case class MultiTree[E <: Position](initialNodes: List[Node[E]])
+final case class UnionTree[E <: Position](initialNodes: List[Node[E]])
     extends EuclideanSearches[E] {}
 
-object MultiTree {
-  def fromTrees[E <: Position](trees: List[QuadTree[E]]): MultiTree[E] =
-    MultiTree[E](trees.map(_.root))
+object UnionTree {
+  def fromTrees[E <: Position](trees: List[QuadTree[E]]): UnionTree[E] =
+    UnionTree[E](trees.map(_.root))
 }
