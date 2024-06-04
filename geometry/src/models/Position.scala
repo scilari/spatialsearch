2b69290209b91098dc687767495378c182a2e83f
Ilari Vallivaara <ilari.vallivaara@gmail.com>
2024-06-04 15:06:29 +0100

    Implicit conversion for convenience

--- geometry/src/models/Position.scala
+++ geometry/src/models/Position.scala
@@ -3,3 +3,9 @@ package com.scilari.geometry.models
 trait Position {
   def position: Float2
 }
+
+object Position {
+  import scala.language.implicitConversions
+
+  implicit def PositionAsFloat2(p: Position): Float2 = p.position
+}
